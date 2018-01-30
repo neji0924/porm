4d75f76e744ec186094deb0ce5b9e3394a9e3f55
Neji <zcon.neji@gmail.com>
2018-01-30 15:17:13 +0800

    file

--- src/resources/views/porm/file.blade.php
+++ src/resources/views/porm/file.blade.php
@@ -7,7 +7,7 @@ if (! isset($errors)) {
     <label for="{{ $name }}" class="col-md-3 col-form-label col-form-label-lg">{{ $label }}</label>
 
     <div class="col-md-8">
-        <input id="{{ $name }}" type="file" name="{{ $name }}" class="{{ $class }}{{ $errors->has($errorName) ? ' is-invalid' : '' }}"  {!! $attr !!} >
+        <input id="{{ $name }}" type="file" value="{{ $value or old($name) }}" name="{{ $name }}" class="{{ $class }}{{ $errors->has($errorName) ? ' is-invalid' : '' }}"  {!! $attr !!} >
 
         <div class="invalid-feedback">
             <strong>{{ $errors->first($errorName) }}</strong>
