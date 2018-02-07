0ef1ccad825f5edfc8cee9990b4a88060350e06f
Neji <zcon.neji@gmail.com>
2018-02-07 17:17:21 +0800

    number

---
+++ src/resources/views/porm/number.blade.php
@@ -0,0 +1,16 @@
+@php
+if (! isset($errors)) {
+    $errors = collect();
+}
+@endphp
+<div class="form-group row">
+    <label for="{{ $name }}" class="col-md-3 col-form-label col-form-label-lg">{{ $label }}</label>
+
+    <div class="col-md-8">
+        <input id="{{ $name }}" type="number" name="{{ $name }}" value="{{ $value or old($name) }}" class="{{ $class }}{{ $errors->has($errorName) ? ' is-invalid' : '' }}" {!! $attr !!} >
+
+        <div class="invalid-feedback">
+            <strong>{{ $errors->first($errorName) }}</strong>
+        </div>
+    </div>
+</div>
